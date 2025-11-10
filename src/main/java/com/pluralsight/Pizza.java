d58107a23248bb7e883a4a5cc22dcd75bf4f627a
Student <wardahjavid62@gmail.com>
2025-11-10 15:06:09 -0500

    Demonstrating inheritance in public class Pizza extends MenuItem, declare/initialize variables,

---
+++ src/main/java/com/pluralsight/Pizza.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza extends MenuItem {
+    private PizzaSize size;
+    private CrustType crust;
+    private boolean stuffedCrust;
