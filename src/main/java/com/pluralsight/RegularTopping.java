d0136336265cf9cfa377aec5e43645a423327886
Student <wardahjavid62@gmail.com>
2025-11-10 14:59:52 -0500

    Demonstrating inheritance in public class RegularTopping extends Topping, override method returns regular toppings has no additional cost.

---
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class RegularTopping extends Topping {
+    public RegularTopping(String name) {
+        super(name, false);
+    }
+
+    @Override
+    public double getPrice(PizzaSize size) {
+        return 0;
+    }
+}
