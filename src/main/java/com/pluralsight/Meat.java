59312aabbc01bfc5b8241cc20115c92589355fd6
Student <wardahjavid62@gmail.com>
2025-11-12 06:23:08 -0500

    Updated public class RegularTopping extends Topping (Inheritance)

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
 
-public class Meat extends PremiumTopping {
-    public Meat(String name, boolean isExtra) {
-        super(name, isExtra, 1.00);
+public class Meat extends Topping {
+    private static final double BASE_PERSONAL = 1.00;
+    public Meat(String name, boolean isExtra) { super(name, isExtra); }
+
+    @Override
+    public double getPrice(PizzaSize size) {
+        double base = switch (size) {
+            case PERSONAL -> BASE_PERSONAL;
+            case MEDIUM -> BASE_PERSONAL * 2;
+            case LARGE -> BASE_PERSONAL * 3;
+        };
+        return base + (isExtra ? BASE_PERSONAL / 2 : 0);
     }
 }
--- src/main/java/com/pluralsight/RegularTopping.java
+++ src/main/java/com/pluralsight/RegularTopping.java
@@ -1,12 +1,6 @@
 package com.pluralsight;
 
 public class RegularTopping extends Topping {
-    public RegularTopping(String name) {
-        super(name, false);
-    }
-
-    @Override
-    public double getPrice(PizzaSize size) {
-        return 0;
-    }
+    public RegularTopping(String name) { super(name, false); }
+    @Override public double getPrice(PizzaSize size) { return 0.0; }
 }
