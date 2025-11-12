110fc826ea92f7b695f0bf155b3cbdfc07c83db0
Student <wardahjavid62@gmail.com>
2025-11-12 05:39:44 -0500

    Updated public class Pizza extends MenuItem (Inheritance)

--- src/main/java/com/pluralsight/Pizza.java
+++ src/main/java/com/pluralsight/Pizza.java
@@ -1,12 +1,17 @@
 package com.pluralsight;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pizza extends MenuItem {
-    private PizzaSize size;
-    private CrustType crust;
-    private boolean stuffedCrust;
-    private List<Topping> toppings = new ArrayList<>();
+    private static final double BASE_PERSONAL = 8.50;
+    private static final double BASE_MEDIUM   = 12.00;
+    private static final double BASE_LARGE    = 16.50;
+
+    private final PizzaSize size;
+    private final CrustType crust;
+    private final boolean stuffedCrust;
+    private final List<Topping> toppings = new ArrayList<>();
 
     public Pizza(PizzaSize size, CrustType crust, boolean stuffedCrust) {
         super("Pizza");
@@ -14,7 +19,33 @@ public class Pizza extends MenuItem {
         this.crust = crust;
         this.stuffedCrust = stuffedCrust;
     }
-    public void addTopping(Topping topping) {
-        toppings.add(topping);
+
+    public PizzaSize getSize() { return size; }
+    public CrustType getCrust() { return crust; }
+    public boolean isStuffedCrust() { return stuffedCrust; }
+    public List<Topping> getToppings() { return toppings; }
+
+    public void addTopping(Topping topping) { toppings.add(topping); }
+
+    @Override
+    public double getPrice() {
+        double base = switch (size) {
+            case PERSONAL -> BASE_PERSONAL;
+            case MEDIUM   -> BASE_MEDIUM;
+            case LARGE    -> BASE_LARGE;
+        };
+        double addOns = 0;
+        for (Topping t : toppings) addOns += t.getPrice(size);
+        return base + addOns;
     }
 
+    @Override
+    public String getLabel() {
+        return size + " " + crust + (stuffedCrust ? " (stuffed)" : "");
+    }
+
+    @Override
+    public String toString() {
+        return getLabel() + " | " + toppings + " | $" + String.format("%.2f", getPrice());
+    }
+}
