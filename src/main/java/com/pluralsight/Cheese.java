7519c424f8c996062977804bb5349255627d65b4
Student <wardahjavid62@gmail.com>
2025-11-12 06:13:16 -0500

    Updated public class Cheese extends Topping (Inheritance)

--- src/main/java/com/pluralsight/Cheese.java
+++ src/main/java/com/pluralsight/Cheese.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
 
-public class Cheese extends PremiumTopping {
-    public Cheese(String name, boolean isExtra) {
-        super(name, isExtra, 0.75);
+public class Cheese extends Topping {
+    private static final double BASE_PERSONAL = 0.75;
+    public Cheese(String name, boolean isExtra) { super(name, isExtra); }
+
+    @Override
+    public double getPrice(PizzaSize size) {
+        double base = switch (size) {
+            case PERSONAL -> BASE_PERSONAL;
+            case MEDIUM -> BASE_PERSONAL * 2;
+            case LARGE -> BASE_PERSONAL * 3;
+        };
+        return base + (isExtra ? BASE_PERSONAL / 2 : 0);
     }
 }
