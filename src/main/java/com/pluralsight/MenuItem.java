1b4552cc90375be58df703de3770861ff2d64557
Student <wardahjavid62@gmail.com>
2025-11-12 05:38:39 -0500

    Updated abstract class MenuItem

--- src/main/java/com/pluralsight/MenuItem.java
+++ src/main/java/com/pluralsight/MenuItem.java
@@ -3,13 +3,12 @@ package com.pluralsight;
 public abstract class MenuItem {
     protected String name;
 
-    public MenuItem(String name) {
-        this.name = name;
-    }
+    protected MenuItem(String name) { this.name = name; }
 
-    public abstract double getPrice();
     public String getCategory() { return name; }
     public String getLabel() { return name; }
+
+    public abstract double getPrice();
     @Override
     public abstract String toString();
 }
