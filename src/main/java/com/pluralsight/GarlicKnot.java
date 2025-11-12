b153664c4fe6c5dac854cd2e43db5111647d6f4d
Student <wardahjavid62@gmail.com>
2025-11-12 06:25:25 -0500

    Created public class GarlicKnot extends MenuItem (Inheritance)

---
+++ src/main/java/com/pluralsight/GarlicKnot.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class GarlicKnot extends MenuItem {
+    private final int quantity;
+    public GarlicKnot(int quantity) { super("GarlicKnot"); this.quantity = quantity; }
+    public int getQuantity() { return quantity; }
+    @Override public double getPrice() { return quantity * 1.50; }
+    @Override public String getLabel() { return quantity + " order(s)"; }
+    @Override public String toString() { return getLabel() + " - $" + String.format("%.2f", getPrice()); }
+}
