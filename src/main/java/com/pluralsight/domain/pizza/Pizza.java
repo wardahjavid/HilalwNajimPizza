9a055f8d3f3c78010305193288ed6b17132cec15
Student <wardahjavid62@gmail.com>
2025-11-13 07:48:13 -0500

    updated classes/packages.

--- src/main/java/com/pluralsight/domain/pizza/Pizza.java
+++ src/main/java/com/pluralsight/domain/pizza/Pizza.java
@@ -8,7 +8,7 @@ import com.pluralsight.domain.topping.Topping;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Pizza extends MenuItem {
+public abstract class Pizza extends MenuItem {
 
     protected PizzaSize size;
     protected CrustType crust;
@@ -16,52 +16,35 @@ public class Pizza extends MenuItem {
 
     protected final List<Topping> toppings = new ArrayList<>();
 
-    public Pizza(PizzaSize size, CrustType crust, boolean stuffedCrust) {
+    protected Pizza(PizzaSize size, CrustType crust, boolean stuffedCrust) {
         super("Pizza");
         this.size = size;
         this.crust = crust;
         this.stuffedCrust = stuffedCrust;
     }
 
-    public PizzaSize getSize() { return size; }
-    public CrustType getCrust() { return crust; }
-    public boolean isStuffedCrust() { return stuffedCrust; }
-    public List<Topping> getToppings() { return toppings; }
-
-    public void addTopping(Topping t) { toppings.add(t); }
-
-    private double getBasePrice() {
-        return switch (size) {
-            case PERSONAL -> 8.50;
-            case MEDIUM   -> 12.00;
-            case LARGE    -> 16.50;
-        };
+    public PizzaSize getSize() {
+        return size;
     }
 
-    private double getStuffedCrustCost() {
-        return stuffedCrust ? 2.00 : 0.00;
+    public CrustType getCrust() {
+        return crust;
     }
 
-    @Override
-    public double getPrice() {
-        double base = getBasePrice();
+    public boolean isStuffedCrust() {
+        return stuffedCrust;
+    }
 
-        double toppingTotal = toppings.stream()
-                .mapToDouble(t -> t.getPrice(size))
-                .sum();
+    public List<Topping> getToppings() {
+        return toppings;
+    }
 
-        return base + toppingTotal + getStuffedCrustCost();
+    public void addTopping(Topping t) {
+        toppings.add(t);
     }
 
     @Override
     public String getLabel() {
         return size + " " + crust + (stuffedCrust ? " (Stuffed)" : "");
-    }
-
-    @Override
-    public String toString() {
-        return "Pizza (" + getLabel() + ")\n" +
-                "Toppings: " + toppings.stream().map(Topping::getName).toList() + "\n" +
-                "Price: $" + String.format("%.2f", getPrice());
     }
 }
